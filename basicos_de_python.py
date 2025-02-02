eafba4e58508156143d0f368df95dcf17e1bd5d9
gabriel-matos23 <gabriel.rocha.matos2005@gmail.com>
2025-02-02 19:23:55 -0300

    Alteração no nome do arquivo

--- basicos_de_python.py
+++ basicos_de_python.py
@@ -4,6 +4,13 @@ adam = 2
 jose = 4
 irineu = "Sou um expert em Python"
 
+
 total_apples = john + adam + jose
 
 print(f"A idade de John é {total_apples} anos")
+
+
+if jose == adam:
+    print("Não")
+else :
+    print("Sim")
