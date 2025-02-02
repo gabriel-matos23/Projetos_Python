d003f653b7c8643eb0b3401a629b74380fa27aaa
gabriel-matos23 <gabriel.rocha.matos2005@gmail.com>
2025-02-02 19:39:16 -0300

    Alterações realizadas dentro do arquivo

--- basicos_de_python.py
+++ basicos_de_python.py
@@ -1,16 +1,9 @@
+nome = input("Digite seu nome: ")
 
-john = 1
-adam = 2
-jose = 4
-irineu = "Sou um expert em Python"
+print(f"Seu nome é {nome}")
 
-
-total_apples = john + adam + jose
-
-print(f"A idade de John é {total_apples} anos")
-
-
-if jose == adam:
-    print("Não")
-else :
-    print("Sim")
+try:
+    idade = int(input("Digite sua idade: "))
+    print(f"Você tem {idade} anos")
+except ValueError:
+    print("Digite um número")
